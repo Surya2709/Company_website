0d1570402a03f28139ff713eb981a3596126b31d
Surya2709 <suryaramachandran27@gmail.com>
2021-03-22 11:34:59 +0530

    modified:   app.py 	templates/

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import render_template
+
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return("hello")
+    return render_template('home.html')
 
 
 def about():
